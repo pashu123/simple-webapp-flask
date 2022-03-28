e9f8519e2b9044edee89414b9a6aa413ffddc166
Prashant Kumar <pk5561@gmail.com>
2022-03-28 12:20:37 +0530

    Updated app.py to reflect html rather than text
    Added script.js, script.css and index.html

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+@app.route('/')
+def home():
+   return render_template('index.html')
 
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
